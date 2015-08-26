479b23e738d8250e661e87daaef7c6e4009360e2
tsmsogn <tsmsogn@gmail.com>
2015-08-26 13:54:43 +0900

    Solve Matrix-like Computation

---
+++ Volume_001/0102_Matrix-like_Computation.rb
@@ -0,0 +1,26 @@
+while n = gets.chomp.to_i
+  break if n == 0
+
+  a = []
+
+  n.times do
+    a << gets.chomp.split.map(&:to_i)
+  end
+
+  a.each do |i|
+    i << i.inject { |sum, n | sum + n }
+  end
+
+  a = a.transpose
+
+  a.each do |i|
+    i << i.inject { |sum, n | sum + n }
+  end
+
+  a = a.transpose
+
+  a.each do |i|
+    puts i.map { |item| sprintf('%5d', item) }.join()
+  end
+
+end
