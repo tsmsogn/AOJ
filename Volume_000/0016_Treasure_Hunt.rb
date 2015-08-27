e6455eb1c2999b19f5a18877d113e9782dd32e5c
tsmsogn <tsmsogn@gmail.com>
2015-08-27 18:07:03 +0900

    Solve Treasure Hunt

---
+++ Volume_000/0016_Treasure_Hunt.rb
@@ -0,0 +1,12 @@
+x, y = 0, 0
+a = 90
+
+while line = gets
+  break if line.chomp == '0,0'
+  c, d = line.chomp.split(',').map(&:to_i)
+  x += c * Math.cos(a * Math::PI / 180)
+  y += c * Math.sin(a * Math::PI / 180)
+  a -= d
+end
+
+puts "#{x.to_i}\n#{y.to_i}"
