242283834f745a59e6a238a159372c4e76c579e6
tsmsogn <tsmsogn@gmail.com>
2015-09-16 10:44:39 +0900

    Remove parenthesis

--- Volume_000/0058_Orthogonal.rb
+++ Volume_000/0058_Orthogonal.rb
@@ -3,6 +3,6 @@ def inner_product(a1, a2, b1, b2)
 end
 
 while line = gets
-  xA, yA, xB, yB, xC, yC, xD, yD = line.chomp.split().map(&:to_f)
+  xA, yA, xB, yB, xC, yC, xD, yD = line.chomp.split.map(&:to_f)
   puts inner_product(xA - xB, yA - yB, xC - xD, yC - yD).abs < 1e-12 ? :YES : :NO
 end
