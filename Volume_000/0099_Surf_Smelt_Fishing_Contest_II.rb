06f8eca0f3f60bc63157932a9a06c4c845096421
tsmsogn <tsmsogn@gmail.com>
2015-10-08 18:23:44 +0900

    Solve Surf Smelt Fishing Contest II

--- Volume_000/0099_Surf_Smelt_Fishing_Contest_II.rb
+++ Volume_000/0099_Surf_Smelt_Fishing_Contest_II.rb
@@ -1,18 +1,30 @@
 hash = Hash.new(0)
+min_a, max_v = Float::INFINITY, -Float::INFINITY
 
 n, q = gets.chomp.split.map(&:to_i)
 
 q.times do
   a, v = gets.chomp.split.map(&:to_i)
   hash[a] += v
-  order = hash.sort do |(k1, v1), (k2, v2)|
-    if v1 == v2
-      k1 <=> k2
-    else
-      v2 <=> v1
+  if v > 0
+    if hash[a] > max_v
+      max_v = hash[a]
+      min_a = a
+    elsif hash[a] == max_v
+      min_a = [a, min_a].min
+    end
+  else
+    if a == min_a
+      order = hash.sort do |(k1, v1), (k2, v2)|
+        if v1 == v2
+          k1 <=> k2
+        else
+          v2 <=> v1
+        end
+      end
+      min_a, max_v = order.first[0], order.first[1]
     end
   end
 
-  first = order.first
-  puts [first[0], first[1]].join(' ')
+  puts [min_a, max_v].join(' ')
 end
