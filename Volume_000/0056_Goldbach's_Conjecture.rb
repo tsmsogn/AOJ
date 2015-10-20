c6290806c9f9d6f66ee6ff980c532beafe9f6fb8
tsmsogn <tsmsogn@gmail.com>
2015-10-20 17:41:45 +0900

    Bug fix, TLE Goldbach's Conjecture

--- Volume_000/0056_Goldbach's_Conjecture.rb
+++ Volume_000/0056_Goldbach's_Conjecture.rb
@@ -1,5 +1,6 @@
 def primes(max)
-  nums = (2..max).each_with_object({}) { |num, hash| hash[num] = 1 }
+  nums = Array.new(max + 1, 1)
+  nums[0] = nums[1] = 0
 
   (2..Math.sqrt(max)).each do |sieve|
     if nums[sieve] == 1
@@ -22,11 +23,9 @@ while line = gets
 
   answer = 0
 
-  if n.even?
-    for i in 2..(n / 2)
-      j = n - i
-      answer += 1 if primes[i] == 1 && primes[j] == 1
-    end
+  for i in 2..(n / 2)
+    j = n - i
+    answer += 1 if primes[i] == 1 && primes[j] == 1
   end
 
   puts answer
