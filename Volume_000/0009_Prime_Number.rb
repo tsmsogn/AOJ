c9e02138483f12abab186c5d798fa46e2af265ad
tsmsogn <tsmsogn@gmail.com>
2015-10-20 17:01:43 +0900

    TLE Prime Number

--- Volume_000/0009_Prime_Number.rb
+++ Volume_000/0009_Prime_Number.rb
@@ -1,7 +1,15 @@
 def primes(max)
-  (2..Math.sqrt(max).ceil).inject([*2..max]) do |nums, sieve|
-    nums.reject { |num| num % sieve == 0 && num != sieve }
+  nums = (2..max).each_with_object({}) { |num, hash| hash[num] = 1 }
+
+  (2..Math.sqrt(max)).each do |sieve|
+    if nums[sieve] == 1
+      (2 * sieve).step(max, sieve).each do |num|
+        nums[num] = 0
+      end
+    end
   end
+
+  nums.select { |key, value| value == 1 }.keys.to_a
 end
 
 while line = gets
