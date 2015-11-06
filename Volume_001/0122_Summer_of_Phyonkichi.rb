79373658e2436869fcbb86a9ee3f9e37f437067e
tsmsogn <tsmsogn@gmail.com>
2015-11-06 16:29:16 +0900

    Solve Summer of Phyonkichi

--- Volume_001/0122_Summer_of_Phyonkichi.rb
+++ Volume_001/0122_Summer_of_Phyonkichi.rb
@@ -1,29 +1,23 @@
-def alive?(splinklers, n, x, y)
-  jump = [
-    [-1, -2], [0, -2], [1, -2],
-    [2, -1], [2, 0], [2, 1],
-    [1, 2], [0, 2], [-1, 2],
-    [-2, 1], [-2, 0], [-2, -1]
-  ]
+JUMP = [
+  [-1, -2], [0, -2], [1, -2],
+  [2, -1], [2, 0], [2, 1],
+  [1, 2], [0, 2], [-1, 2],
+  [-2, 1], [-2, 0], [-2, -1]
+]
 
-  stack = Array.new(n + 1) { [] }
-  stack[0] << [x, y]
+def dfs(x, y, splinklers, n, depth = 0)
+  return true if depth == n
 
-  for i in 0...n
-    until stack[i].empty?
-      p = stack[i].pop
-      for j in 0...jump.size
-        _x = p[0] + jump[j][0]
-        _y = p[1] + jump[j][1]
-        d = (_x - splinklers[i][0])**2 + (_y - splinklers[i][1])**2
-        if 0 <= _x && _x < 10 && 0 <= _y && _y < 10 && d <= 2
-          stack[i + 1] << [_x, _y]
-        end
-      end
+  for i in 0...JUMP.size
+    _x = x + JUMP[i][0]
+    _y = y + JUMP[i][1]
+    d = (_x - splinklers[depth][0])**2 + (_y - splinklers[depth][1])**2
+    if 0 <= _x && _x < 10 && 0 <= _y && _y < 10 && d <= 2
+      return true if dfs(_x, _y, splinklers, n, depth + 1)
     end
   end
 
-  stack[10].any?
+  false
 end
 
 while line = gets
@@ -36,5 +30,5 @@ while line = gets
     splinklers << data.slice(i * 2, 2)
   end
 
-  puts (alive?(splinklers, n, x, y)) ? :OK : :NA
+  puts (dfs(x, y, splinklers, n)) ? :OK : :NA
 end
