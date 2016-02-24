52b845c498c0a668e82b6cef11333d1fad439cb4
tsmsogn <tsmsogn@gmail.com>
2016-02-24 10:44:23 +0900

    Clean up codes

--- Volume_000/0044_Prime_Number_II.rb
+++ Volume_000/0044_Prime_Number_II.rb
@@ -1,6 +1,6 @@
 max = 50021
 
-primes = (2..Math.sqrt(max).ceil).inject([*2..max]) do |nums, sieve|
+primes = (2..Math.sqrt(max)).inject([*2..max]) do |nums, sieve|
   nums.reject { |num| num % sieve == 0 && num != sieve }
 end
 
