bc5e6ff7fe3313a9ee6be63bceb77fab0c58e263
tsmsogn <tsmsogn@gmail.com>
2016-05-14 23:49:04 +0900

    Solve GCD and LCM

---
+++ Volume_000/0005_GCD_and_LCM.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (a < b) {
+        swap(a, b);
+    }
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+int main() {
+    int a, b;
+
+    while (cin >> a >> b) {
+        cout << gcd(a, b) << " " << lcm(a, b) << endl;
+    }
+    return 0;
+}
