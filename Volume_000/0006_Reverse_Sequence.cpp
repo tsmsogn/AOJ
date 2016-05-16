dd95ca6fe8586cbcb25ff57997ed9c887eb6fc19
tsmsogn <tsmsogn@gmail.com>
2016-05-16 23:32:34 +0900

    Solve Reverse Sequence

---
+++ Volume_000/0006_Reverse_Sequence.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string str;
+
+    cin >> str;
+
+    reverse(str.begin(), str.end());
+
+    cout << str << endl;
+
+    return 0;
+}
